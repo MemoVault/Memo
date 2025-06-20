96b1b3b00f2668933ede672b13c9609f4c787215
MemoVault <max.galkow@gmail.com>
2025-06-20 23:57:30 +0200

    Delete pages/index.tsx

--- pages/index.tsx
+++
@@ -1,9 +0,0 @@
-// pages/index.tsx
-export default function Home() {
-  return (
-    <div style={{ padding: "2rem", fontFamily: "Arial" }}>
-      <h1>Welcome to MemoVault</h1>
-      <p>Your private memory assistant is coming soon...</p>
-    </div>
-  );
-}
